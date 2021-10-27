c9826febf0b780dff639dc50588d663518e561e1
doogeemodulny <doogee.modulny@gmail.com>
2021-10-27 03:14:29 +0300

    first_commit

---
+++ polls/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from .models import Question
+from django.template import loader
+
+
+def index(request):
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_questions
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    return HttpResponse('You are looking at question %s' % question_id)
+
+
+def result(request, question_id):
+    return HttpResponse("You're looking at the results of question %s." % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
