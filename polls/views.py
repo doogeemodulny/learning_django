e1cfe7c5dd3dd1527df13d4683a2ca45d9334690
doogeemodulny <doogee.modulny@gmail.com>
2021-10-27 04:19:58 +0300

    lesson3

--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,18 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Question
 from django.template import loader
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
-    latest_questions = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_questions
-    }
-    return HttpResponse(template.render(context, request))
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.ru', context)
 
 
 def detail(request, question_id):
-    return HttpResponse('You are looking at question %s' % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def result(request, question_id):
